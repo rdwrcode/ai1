9d51d08b7e368901301872bc60c06a5c64e91bfd
rdwrcode <rdwrcode@gmail.com>
2016-10-26 10:11:58 -0600

    4-extra

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Match, Miss, Link } from 'react-router';
 import Home from './home/Home';
-import CSSMotion from './animation/CSSMotion';
-import Menu from './sidebar/Menu';
 import About from './about/About';
 import NotFound from './NotFound';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -14,20 +11,12 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-          </div>
           <ul className="App-list">
             <li><Link to="/">Home</Link></li>
-            <li><Link to="/cssmotion">CSSMotion</Link></li>
-            <li><Link to="/menu">Menu</Link></li>
             <li><Link to="/about">About</Link></li>
           </ul>
           <hr />
           <Match exactly pattern="/" component={Home} />
-          <Match pattern="/cssmotion" component={CSSMotion} />
-          <Match pattern="/menu" component={Menu} />
           <Match pattern="/about" component={About} />
           <Miss component={NotFound}/>
         </div>
--- src/about/About.js
+++ src/about/About.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import './About.css';
+import logo from './logo.svg';
 
 const About = () => (
-  <div>
-    <h2>About</h2>
+  <div className="about">
+    <div className="about-header">
+      <img src={logo} className="logo" alt="logo" />
+      <h2>Welcome to React</h2>
+    </div>
   </div>
 )
 
